ec699f9ba240ee2203e8032406d9d6eb2dd2b6c9
Sadisa123c <sadisaiinduwara@gmail.com>
2024-09-23 12:36:41 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&2JqN1AWT",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&x5Fb4x8f",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
